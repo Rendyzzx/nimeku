d076509ba505fac950347505a9b4cc0e6a2bec97
Rendyzzx <101921601+Rendyzzx@users.noreply.github.com>
2026-08-09 02:02:39 +0700

    Tambah API: manga (blank)

---
+++ api/manga.js
@@ -0,0 +1,4 @@
+// Auto-generated via Telegram bot. Edit sesuai kebutuhan.
+export default async function handler(req, res) {
+  return res.status(200).json({ success: true, message: 'Hello from manga' });
+}
